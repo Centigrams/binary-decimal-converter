893bcf0e43888c42446237e6d595c9a945a8fb3f
Sidd Bairan <bairansa@gmail.com>
2021-10-21 22:43:21 +0800

    Transfer index.js entry point to new scripts directory

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/scripts/index.js',
     mode: 'development',
     devtool: 'inline-source-map',
     output: {
